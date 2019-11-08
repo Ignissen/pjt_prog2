0630eef25f12b4e238c22adc355dd729c3b5e273
ignissen <jozsefpetrus99@gmail.com>
2019-11-08 01:53:28 +0100

    godel/php updated

--- godel/bonus_skip2_udprog/stop.php
+++ godel/bonus_skip2_udprog/stop.php
@@ -2,11 +2,11 @@
     session_start();
     if(isset($_GET["time"]))
     {
-        $file = fopen("/tmp/osszesitett.txt", "w");
-        fwrite($file, $_GET["time"]);
-        fclose($file);
         $file = fopen("/tmp/".$_SESSION["hackername"].".txt", "w");
-        
+        $time = file_get_contents("/tmp/".$_SESSION["hackername"].".txt");
+        fwrite($file,intval($time) + intval($_GET["time"]));
+        fclose($file);
+
         $n_of_hackers = intval(file_get_contents("/tmp/num_of_hackers.txt")) - 1;
         
         if($n_of_hackers > 0)
@@ -21,9 +21,9 @@
     }
     if(isset($_GET["sumtime"]))
     {
-        $filename = "/tmp/" . $_SESSION["hackername"] . ".txt";
+        $filename = "/tmp/osszesitett.txt";
         $sum_file = fopen($filename,"w");
-        fwrite($sum_file, $_GET["sumtime"] + intval(file_get_contents($filename)));
+        fwrite($sum_file, intval($_GET["sumtime"]) + intval(file_get_contents($filename)));
         fclose($sum_file);
     }
     session_destroy();
